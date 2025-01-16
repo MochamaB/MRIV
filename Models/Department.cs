06e735c7359090971897b7be674bc1f708f3ab1f
Brian Mochama <brianmochama@gmail.com>
2025-01-16 20:06:38 +0300

    Initial Commit

---
+++ Models/Department.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MRIV.Models;
+
+[Table("Department")]
+public partial class Department
+{
+    [Key]
+    [Column("departmentCode")]
+    public int DepartmentCode { get; set; }
+
+    [Column("DepartmentID")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? DepartmentId { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string DepartmentName { get; set; } = null!;
+
+    [Column("DepartmentHD")]
+    [StringLength(50)]
+    public string? DepartmentHd { get; set; }
+
+    [StringLength(50)]
+    public string? Emailaddress { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? UserName { get; set; }
+
+    [StringLength(5)]
+    [Unicode(false)]
+    public string? OrgCode { get; set; }
+}
